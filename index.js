cf0143fccee17c2200e7b7d429423bf914b4cc0d
Louis Coleman <coleloui18@gmail.com>
2020-04-02 20:24:17 -0700

    readme generator

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const axios = require("axios");
 
 
 inquirer
     .prompt([{
-        type: "input",
-        message: "What is the title of your project?",
-        name: "project"
-    }, {
-        type: "input",
-        message: "Give a brief description of your project.",
-        name: "description"
-    }, {
-        type: "input",
-        message: "What does your user need to install to use your project?",
-        name: "install"
-    }, {
-        type: "input",
-        message: "What is the use of your project?",
-        name: "usage"
-    }, {
-        type: "input",
-        message: "What is the license for your project?",
-        name: "license"
-    }, {
-        type: "input",
-        message: "Who contributed on this project?",
-        name: "help"
-    }, {
-        type: "input",
-        message: "What tests did you run on this project?",
-        name: "test"
-    }, {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "git"
-    }])
+            type: "input",
+            message: "What is the title of your project?",
+            name: "project"
+        }, {
+            type: "input",
+            message: "Give a brief description of your project.",
+            name: "description"
+        }, {
+            type: "input",
+            message: "What does your user need to install to use your project?",
+            name: "install"
+        }, {
+            type: "input",
+            message: "What is the use of your project?",
+            name: "usage"
+        }, {
+            type: "input",
+            message: "What is the license for your project?",
+            name: "license"
+        }, {
+            type: "input",
+            message: "Who contributed on this project?",
+            name: "help"
+        }, {
+            type: "input",
+            message: "What tests did you run on this project?",
+            name: "test"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "git"
+        }
+    ])
     .then(function (response) {
 
         const {
@@ -49,44 +52,67 @@ inquirer
             git
         } = response;
 
-        
-        const myREADME =
-            `
+        const gitHub = "https://github.com/" + git;
+        const gitAPI = "https://api.github.com/users/" + git;
 
-            # ** ${project} **
-            ---
-            ## Description
-            ${description}
-            ---
-            ## Table of Contents
-            *[Description](#description)
-            *[Installation](#installation)
-            *[Contributors](#contributors)
-            ---
-            ## Installation Guide
-            ${install}
-            ---
-            ## Usage for My Project
-            ${usage}
-            ---
-            ## ${license}
-            ---
-            ## Contributors and Help
-            ${help}
-            ---
-            ## Tests
-            ${test}
-            ---
-            ## GitHub
-            ${git}
-            ---
-            
-            `
-        fs.writeFile("testREADME.md", myREADME, function (err) {
-            if (err) {
-                return console.log("We did not process your request " + err);
-            } else {
-                return console.log("Enjoy your new README!");
-            }
-        })
+        axios
+            .get(gitAPI).then(function (response) {
+                const pic = response.data.avatar_url;
+                let userEmail = response.data.email;
+                if (response.data.email == null) {
+                    userEmail = "No user email";
+                };
+
+                const myREADME =
+                    `
+        
+# **${project}** 
+
+![badge](https://img.shields.io/badge/license-${license}-brightgreen)
+---
+## Description
+${description}
+---
+## Table of Contents
+*[Description](#description)
+
+*[Installation](#installation)
+
+*[GitHub](#github)
+---
+## Installation Guide
+${install}
+---
+## Usage for My Project
+${usage}
+---
+## Contributors and Help
+${help}
+---
+## Tests
+${test}
+---
+## GitHub
+![user github pic](${pic})
+
+
+${gitHub}
+
+
+${userEmail}
+---
+                    
+                    `
+                makeReadMe(myREADME);
+            })
+
+        function makeReadMe(myREADME) {
+            fs.writeFile("testREADME.md", myREADME, function (err) {
+                if (err) {
+                    return console.log("We did not process your request " + err);
+                } else {
+                    return console.log("Enjoy your new README!");
+                }
+            })
+        }
     });
---
+++ testREADME.md
@@ -0,0 +1,39 @@
+
+        
+# **readme** 
+
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+---
+## Description
+it makes a read me
+---
+## Table of Contents
+*[Description](#description)
+
+*[Installation](#installation)
+
+*[GitHub](#github)
+---
+## Installation Guide
+just npm i
+---
+## Usage for My Project
+to make a read me
+---
+## Contributors and Help
+me
+---
+## Tests
+none
+---
+## GitHub
+![user github pic](https://avatars0.githubusercontent.com/u/16417094?v=4)
+
+
+https://github.com/coleloui
+
+
+No user email
+---
+                    
+                    
